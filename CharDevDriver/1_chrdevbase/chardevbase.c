5e095416a6c632b73cfe44c552218ebe1dabec1c
yihoom <13699489318@163.com>
2024-09-29 05:25:47 -0700

    第一个虚拟驱动模块

---
+++ CharDevDriver/1_chrdevbase/chardevbase.c
@@ -0,0 +1,19 @@
+#include <linux/module.h>
+
+static int __init chardevbase_init(void)
+{
+
+	return 0;
+}
+
+static void __exit chardevbase_exit(void)
+{
+
+}
+
+
+/**
+ * 驱动模块的入口与出口
+*/
+module_init(chardevbase_init);  //入口，加载模块时执行
+module_exit(chardevbase_exit);  //出口，卸载模块时执行
