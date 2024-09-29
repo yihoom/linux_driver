cf5b91a3cec36a7eceac89e653aa20d2ca3ebe84
yihoom <13699489318@163.com>
2024-09-29 05:27:24 -0700

    添加license

--- CharDevDriver/1_chrdevbase/chardevbase.c
+++ CharDevDriver/1_chrdevbase/chardevbase.c
@@ -17,3 +17,6 @@ static void __exit chardevbase_exit(void)
 */
 module_init(chardevbase_init);  //入口，加载模块时执行
 module_exit(chardevbase_exit);  //出口，卸载模块时执行
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("JYH");
