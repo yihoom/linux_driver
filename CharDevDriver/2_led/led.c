d742e5798f3d7784c98e5e6eeb11ef069bfc487b
yihoom <13699489318@163.com>
2024-10-17 06:57:03 -0700

    完成了led驱动与led测试app的编写

--- CharDevDriver/2_led/led.c
+++ CharDevDriver/2_led/led.c
@@ -22,6 +22,28 @@ static void __iomem *SW_PAD_GPIO1_IO03;
 static void __iomem *GPIO1_DR;
 static void __iomem *GPIO1_GDIR	;
 
+#define LEDOFF 	0  	/*关闭*/
+#define LEDON 	1	/*打开*/
+
+
+static void LED_Switch(u8 sta)
+{
+	u32 val;
+	if(sta == LEDOFF)
+	{
+		val = readl(GPIO1_DR);
+		val |= (1 << 3);			/*关闭LED灯*/
+		writel(val, GPIO1_DR);
+	}
+	else if (sta == LEDON)
+	{
+		val = readl(GPIO1_DR);
+		val &= ~(1 << 3);			/*bit3清零，打开LED灯*/
+		writel(val, GPIO1_DR);
+	}
+	
+} 
+
 static int led_open(struct inode *inode, struct file *file)
 {
 	// printk("chrdevbase_open\r\n");
@@ -30,14 +52,28 @@ static int led_open(struct inode *inode, struct file *file)
 
 static int led_release(struct inode *inode, struct file *file)
 {
-	// printk("chrdevbase_open\r\n");
 	return 0;
 }
 
 static ssize_t led_write(struct file *file, const char __user *user_buf,
 			       size_t length, loff_t *offset)
 {
-	// printk("chrdevbase_open\r\n");
+	int ret;
+	u8 databuf[1];
+
+	ret = copy_from_user(databuf, user_buf, length);
+
+	if(ret < 0)
+	{
+		printk("kernel write failed\r\n");
+		return -EFAULT;
+	}
+
+	/*判断是开灯还是关灯*/
+	LED_Switch(databuf[0]);
+
+
+
 	return 0;
 }
 
@@ -54,7 +90,7 @@ static struct file_operations sLed_fops = {
 static int __init led_init(void)
 {
 	int ret = 0;
-	int val = 0;
+	u32 val = 0;
 	/* 1.初始化LED灯,地址映射 */
 	IMX6U_CCm_CCGR1 = ioremap(CCM_CCGR1_BASE, 4);
 	SW_MUX_GPIO1_IO03 = ioremap(SW_MUX_GPIO1_IO03_BASE, 4);
@@ -95,7 +131,7 @@ static int __init led_init(void)
 
 static void __exit led_exit(void)
 {
-	int val;
+	u32 val;
 
 	val = readl(GPIO1_DR);
 	val |= (1 << 3);			/*关闭LED灯*/
--- CharDevDriver/2_led/ledApp.c
+++ CharDevDriver/2_led/ledApp.c
@@ -8,90 +8,57 @@
 /**
  * argc:应用程序参数的个数
  * argv[]:具体的参数内容，字符串形式
- * ./chrdevbaseApp <filename> <1:2> 1表示读，2表示写
+ * ./ledApp <filename> <0:1> 0表示关灯，1表示开灯
 */
+
+#define LEDOFF 	0  	/*关闭*/
+#define LEDON 	1	/*打开*/
+
 int main(int argc, char *argv[])
 {
-    int fd = 0;
-    int ret = 0;
     char *filename;
-    char *writeparam;
-    char Readbuf[100];
-    char writebuf[100];
-    static char userdara[] = {"user data"};
-
-    int OperaFalg = 0;
-
+    int fd;
+    int ret;
+    int stat;
+    char writebuf;
     if(argc != 3)
     {
-        printf("error cmd\r\n");
+        printf("error CMD");
         return -1;
     }
 
     filename = argv[1];
-    OperaFalg = atoi(argv[2]);
+    stat = atoi(argv[2]);
 
+    /*1.打开驱动*/
+    fd = open(filename , O_RDWR);
 
-    //open返回一个文件描述符
-    //文件操作符详见man 2 open
-    fd = open(filename, O_RDWR);
-
-    //open如果出错，则会返回-1
     if(fd < 0)
     {
-        printf("Can't open file %s\r\n", filename);
+        printf("open file failed\r\n");
         return -1;
     }
 
-    //read函数
-    //如果成功的话，read会返回读到的字节数
-    //ssize_t read(int fd, void *buf, size_t count);
+    /*2.写驱动*/
 
-    if(OperaFalg == 1)
+    if(stat == LEDON)
     {
-
-        ret = read(fd, Readbuf, 50);
-    
-        if(ret == 0)
-        {
-            printf("read from driver %s\r\n", Readbuf);
-        }
-
-        //read函数如果失败，会返回-1
-        if(ret < 0)
-        {
-            printf("read file %s failed\r\n", filename);
-        }
-    
+        writebuf = LEDON;
+        ret = write(fd, &writebuf, 1);
     }
-    
-
-    /* wrirte函数 */
-    //ssize_t write(int fd, const void *buf, size_t count);
-    //如果成功write会返回写入的字节数
-    if(OperaFalg == 2)
-    {   
-        memcpy(writebuf, userdara, sizeof(userdara));
-        ret = write(fd, writebuf, 20);
-
-        //如果错误，会返回-1
-        if(ret < 0)
-        {
-            printf("write to file %s failed\r\n", filename);
-        }
-        
-    }
-    
-
-    /* close */
-    //  int close(int fd);
-    // close成功的话返回0，失败的话返回-1
-    ret = close(fd);
-    if(ret < 0)
+    else if(stat == LEDOFF)
     {
-        printf("close file %s failed\r\n", filename);
+        writebuf = LEDOFF;
+        ret = write(fd, &writebuf, 1);
+    }
+    else
+    {
+        printf("3rd arg wrong\r\n");
     }
 
+    /*关闭驱动*/
+
+    close(fd);
 
     return 0;
 }
