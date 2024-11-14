300b205b9d7bd20c0e8d01a028dd02a8e41f8fff
yihoom <13699489318@163.com>
2024-11-14 06:13:26 -0800

    开始蜂鸣器的开发

---
+++ CharDevDriver/7_beep/beep.c
@@ -0,0 +1,16 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/fs.h>
+#include <linux/uaccess.h>
+#include <linux/io.h>
+#include <linux/cdev.h>
+#include <linux/device.h>
+#include <linux/of.h>
+#include <linux/of_address.h>
+#include <linux/of_irq.h>
+#include <linux/slab.h>
+#include <linux/of_gpio.h>
+
+
+
