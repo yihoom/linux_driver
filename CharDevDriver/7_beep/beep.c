ba5375db5a741fa90a9dcd43ea3bae1610760a29
yihoom <13699489318@163.com>
2024-11-14 07:17:04 -0800

    完成了蜂鸣器驱动的编写

--- CharDevDriver/7_beep/beep.c
+++ CharDevDriver/7_beep/beep.c
@@ -47,6 +47,11 @@ ssize_t gpioled_write (struct file *file, const char __user *user_buf,
 			       size_t length, loff_t *offset)
 {
     struct beep_dev *dev = (struct beep_dev *)file->private_data;
+
+    u8 databuf;
+    int ret;
+    ret = copy_from_user(&databuf, user_buf, length);
+    gpio_set_value(gpioled.beep_gpio, !databuf);
     return 0;
 }
 
@@ -118,10 +123,46 @@ static int __init gpioled_init(void)
         goto fail_device;
     }
     
+    /*初始化设备节点*/
+    gpioled.dev_nd = of_find_node_by_path("/beepled");
+    if(gpioled.dev_nd == NULL)
+    {
+        ret = -EINVAL;
+        goto fail_node;
+    }
 
+    /*找到gpio*/
+    gpioled.beep_gpio = of_get_named_gpio(gpioled.dev_nd, "beep-gpios", 0);
+    if(gpioled.beep_gpio < 0)
+    {
+        ret = gpioled.beep_gpio;
+        goto fail_gpio;
+    }
+    printk("gpio num = %d\r\n", gpioled.beep_gpio);
+    
+    /*申请GPIO*/
+    ret = gpio_request(gpioled.beep_gpio, "beep_gpio3");
+    if(ret < 0)
+    {
+        printk("cannot request gpio\r\n");
+        goto fail_gpio_request;
+    }
+
+    ret = gpio_direction_output(gpioled.beep_gpio, 0);
+    if(ret < 0)
+    {
+        goto fail_gpio_dir;
+    }
     return 0;
 
+    gpio_set_value(gpioled.beep_gpio, 0);
 
+fail_gpio_dir:
+    gpio_free(gpioled.beep_gpio);
+fail_gpio_request:
+fail_gpio:
+fail_node:
+    device_destroy(gpioled.class, gpioled.devid);
 
 fail_device:
     class_destroy(gpioled.class);
@@ -140,7 +181,9 @@ fail_devid:
 
 static void __exit gpioled_exit(void)
 {
-
+    gpio_set_value(gpioled.beep_gpio, 1);
+    /*释放GPIO*/
+    gpio_free(gpioled.beep_gpio);
     /*删除设备*/
     device_destroy(gpioled.class, gpioled.devid);
     /*删除类*/
