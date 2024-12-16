f31d65fecb347e073560b5e55b9f0085e7f66c31
yihoom <13699489318@163.com>
2024-12-16 05:07:53 -0800

    完成了唤醒与等待

---
+++ CharDevDriver/16_blockio/keyAPP.c
@@ -0,0 +1,66 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * argc:应用程序参数的个数
+ * argv[]:具体的参数内容，字符串形式
+ * ./keyApp <filename> 
+*/
+
+#define KEYVAL    0xF0  
+#define INVALKEY  0x0
+
+int main(int argc, char *argv[])
+{
+    char *filename;
+    int fd;
+    int ret;
+    unsigned char value;
+    char writebuf;
+    if(argc != 2)
+    {
+        printf("error CMD");
+        return -1;
+    }
+
+    filename = argv[1];
+
+    /*1.打开驱动*/
+    fd = open(filename , O_RDWR);
+
+    if(fd < 0)
+    {
+        printf("open file failed\r\n");
+        return -1;
+    }
+
+    /*2。循环读取按键指*/
+    while (1)
+    {
+        ret = read(fd, &value, sizeof(value));
+        if(ret < 0)
+        {
+
+        }
+        else
+        {
+            if(value)
+            {
+                printf("KEY Pressed, value=%d\r\n", value);
+            }
+        }
+        
+    }
+    
+
+
+    /*关闭驱动*/
+
+    close(fd);
+
+    return 0;
+}
